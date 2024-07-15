1effb37eb839a4a6c8e9b49cb2cf167d00099217
Graveto <graveto.rogerio@gmail.com>
2024-07-15 16:31:17 -0300

    atualizei o config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const config = {
-    linkGoogleScript: 'https://script.google.com/macros/s/AKfycbyy_HlKWIR7fJ5FWKQ905uR56LLLtlmUpxaJhnwpSCNEUuHrNalvsF5KFPNHE7UHn4iYg/exec',
-    linkGoogleScript2: 'https://script.google.com/macros/s/AKfycbwoWOO8i3-t8xIfT6fm57RJ37-1ng0n7kslE3Z_DUCQk6oa4QEpNeW-NGri5TTWl8OY/exec',
-    link: 'https://docs.google.com/spreadsheets/d/1ama0OQ4TFMJdaZHP-TNJvWGGKxn8l1_QWaD6WbRplHo/edit?gid=0#gid=0',
+    linkGoogleScript: 'https://script.google.com/macros/s/AKfycbxrwtHnYiNoXwlHbgoTysCSjN96SbJsB01lZwJAx8nQARJminMfO0uYJgErDx87rt5r/exec',
+    linkGoogleScript2: 'https://script.google.com/macros/s/AKfycbzsBATZiI4-Lu3OaplYFPZFUSBA8HzeaueU-I7ZSWDXzgy5zQ402VrlYrlUk_tZ-vw0/exec',
+    link: 'https://docs.google.com/spreadsheets/d/1h2j3jyU6bqbH5ESbfSkwEhqOB5lsjKQl2qZM6TkPtlo/edit?gid=0#gid=0',
     paginaEstoque: 'Estoque',
     paginaVendas: 'Vendas',
     paginaLogin: 'User',
